23f1fcea608c50fb814aff5008b78fd92ea0f739
tangweikun <819573105@qq.com>
2020-08-26 17:31:58 +0800

    update services

--- cloudfunctions/services/db.md
+++ cloudfunctions/services/db.md
@@ -7,4 +7,34 @@
 | question  | Array   | 问题         |
 | gameplay  | String  | 类型         |
 
-- `gameplay`取值：`SUI_BIAN_WAN_WAN`
+- `gameplay`取值：`SUI_BIAN_WAN_WAN`, `GUO_GUAN_ZHAN_JIANG`、`FENG_MIAO_BI_ZHENG`
+
+## guo_guan_zhan_jiang
+
+| 字段名    | 类型   | 备注     |
+| --------- | ------ | -------- |
+| userInfo  | Object | 用户信息 |
+| createdAt | Date   | 创建日期 |
+| record    | Number | 记录     |
+
+## feng_miao_bi_zheng
+
+| 字段名    | 类型   | 备注     |
+| --------- | ------ | -------- |
+| userInfo  | Object | 用户信息 |
+| createdAt | Date   | 创建日期 |
+| record    | Number | 记录     |
+| totalTime | Number | 总用时   |
+
+## points
+
+| 字段名    | 类型   | 备注     |
+| --------- | ------ | -------- |
+| userInfo  | Object | 用户信息 |
+| createdAt | Date   | 创建日期 |
+| updatedAt | Date   | 更新日期 |
+| points    | Number | 积分     |
+
+答对一题 +1
+过关斩将完成一把 +1
+过关斩将加分规则 5 -> 5; 10 -> 15; 20 -> 40; 30 -> 90; 40 -> 160; 50 -> 250; 60 -> 360
--- cloudfunctions/services/index.js
+++ cloudfunctions/services/index.js
@@ -6,13 +6,16 @@ cloud.init({
 });
 
 exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext();
   const action = event.data.action;
   const payload = event.data.payload;
+  const _ = db.command;
 
   const db = wx.cloud.database();
   const GGZJ_DB = db.collection('guo_guan_zhan_jiang'); // 过关斩将
   const FMBZ_DB = db.collection('feng_miao_bi_zheng'); // 分秒必争
   const QUESTION_DB = db.collection('question'); // 答题记录
+  const POINTS_DB = db.collection('points'); // 积分
 
   // --------------------------------------------------
 
@@ -73,5 +76,39 @@ exports.main = async (event, context) => {
         gameplay: payload.gameplay,
       },
     });
+  }
+
+  // --------------------------------------------------------------
+  // 添加或更新积分记录
+  if (action === 'UPDATE_POINTS') {
+    const res = await POINTS_DB.where({ _openid: wxContext.OPENID }).count();
+    if (res.total === 0) {
+      POINTS_DB.add({
+        data: {
+          userInfo: payload.userInfo,
+          points: payload.points,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      });
+    }
+
+    return await POINTS_DB.where({ _openid: wxContext.OPENID }).update({
+      data: {
+        points: _.inc(payload.points),
+      },
+    });
+  }
+
+  // 查询积分
+  if (action === 'GET_POINTS') {
+    return await POINTS_DB.where({ _openid: wxContext.OPENID }).get();
+  }
+
+  // 查询积分排名
+  if (action === 'GET_POINTS_RANK') {
+    const res = await POINTS_DB.where({ _openid: wxContext.OPENID }).get();
+    const points = res.points;
+    return await POINTS_DB.where({ points: _.gt(points) }).count();
   }
 };
